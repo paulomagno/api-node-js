a1a0fca8a150e671ce8789750f4c38a8be0aabd3
Paulo Miranda <paulo2305@gmail.com>
2021-01-28 14:35:16 -0300

    upload para git

---
+++ src/controllers/NoteController.js
@@ -0,0 +1,93 @@
+const NoteService = require('../services/NoteService');
+
+module.exports = {
+    ping : (req,res) => {
+        res.json({pong:true});
+    },
+    all: async (req,res) => {
+        
+        let json  = {error: '', result:[]};
+        let notes = await NoteService.getAll();
+
+        for (let i in notes) {
+            json.result.push({
+                id: notes[i].id,
+                title: notes[i].title
+            });
+            
+        }
+        
+        res.json(json);
+    },
+    one: async (req,res) => {
+        
+        let json = {error: '', result:{}};
+        let id   = req.params.id;
+        
+        let note = await NoteService.findById(id);
+       
+        if(note) {
+            json.result = note;
+        }
+        
+        res.json(note);
+    },
+    new: async (req,res) => {
+        let json = {error: '', result:{}};
+
+        let title = req.body.title;
+        let body  = req.body.body;
+
+        if(title && body) {
+            
+            let noteId = await NoteService.add(title,body);
+            
+            json.result = {
+                id: noteId,
+                title: title,
+                body:body
+            }
+        }else {
+            json.error = 'Campos não enviados';
+        }
+
+        res.json(json);
+    },
+    edit: async (req,res) => {
+
+        let json = {error:'', result:{}};
+
+        let id = req.params.id;
+        let title = req.body.title;
+        let body = req.body.body;
+
+        if(title && body && id) {
+            
+            let dataUpdate = {title:title,body:body};
+            
+            await NoteService.update(id,dataUpdate);
+
+            json.result = {
+                id: id,
+                title: title,
+                body:body
+            }
+        }
+        else {
+            json.error = 'Campos não enviados';
+        }
+
+        res.json(json);
+
+    },
+    delete : async (req,res) => {
+        let json = {error : '', result:{}};
+
+        await NoteService.delete(req.params.id);
+        json.result = {
+            id: req.params.id
+        }
+
+        res.json(json);
+    }
+};
---
+++ src/services/NoteService.js
@@ -0,0 +1,83 @@
+const db = require('../db/db');
+
+module.exports = {
+    
+    getAll:  () => {
+        return new Promise ((resolve,reject) => {
+            
+            db.query('SELECT *FROM notes',(error,results) => {
+                if(error) { 
+                    reject(error); return;
+                }
+                resolve(results);
+            });
+        });
+    },
+
+    findById: (id) => {
+        
+        return new Promise((resolve,reject) => {
+            db.query('SELECT *FROM notes WHERE id = ?',[id],(error,results) => {
+                if(error) {
+                    reject(error); 
+                    return;
+                }
+                if(results.length > 0) {
+                    resolve(results[0]);
+                } else {
+                    resolve(false);
+                }
+               
+            });
+        });
+    },
+
+    add: (title,body) => {
+        return new Promise((resolve,reject) => {
+            
+            db.query('INSERT INTO notes (title,body) VALUES (?,?)',
+                [title,body],
+                (error,results) => {
+                
+                if(error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(results.insertId);
+                
+            });
+        });
+    },
+
+    update : (id,dataUpdate) => {
+        return new Promise((resolve,reject) => {
+            
+            db.query('UPDATE notes SET title = ? , body=? WHERE id=?',
+                [dataUpdate.title,dataUpdate.body,id],
+                (error,results) => {
+                
+                if(error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(results);
+                
+            });
+        });
+    },
+
+    delete : (id) => {
+        
+        return new Promise((resolve,reject) => {
+            db.query('DELETE FROM notes WHERE id=?',[id],(error,results) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(results);
+            });
+        });
+    }
+};
